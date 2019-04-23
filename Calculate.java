5a951bbe4ac40174aedeb55c552d84ea7c88e1aa
zscott1w <48800336+zscott1w@users.noreply.github.com>
2019-04-23 10:32:32 -0400

    Add files via upload

---
+++ Calculate.java
@@ -0,0 +1,143 @@
+public class Calculate extends Matrix {
+	
+	public Calculate() {
+		super();
+	}
+	
+    public Matrix add(Matrix n, Matrix m) {
+        // Note: can only be added if same dimension!
+        int[][] resultArray = new int[m.getRows()][m.getColumns()];
+        int nData;
+        int mData;
+        int sum;
+        for (int row = 0; row < m.getRows(); row++) {
+            for (int col = 0; col < m.getColumns(); col++) {
+                nData = n.getNumber(row, col);
+                mData = m.getNumber(row, col);
+                sum = nData + mData;
+                resultArray[row][col] = sum;
+            }
+        }
+        return new Matrix(resultArray);
+    }
+
+    public int add(int n, int m){
+        return n + m;
+    }
+
+    public Matrix subtract(Matrix n, Matrix m) {
+        // Note: can only be added if same dimension!
+        int[][] resultArray = new int[m.getRows()][m.getColumns()];
+        int nData;
+        int mData;
+        int sum;
+        for (int row = 0; row < m.getRows(); row++) {
+            for (int col = 0; col < m.getColumns(); col++) {
+                nData = n.getNumber(row, col);
+                mData = m.getNumber(row, col);
+                sum = nData - mData;
+                resultArray[row][col] = sum;
+            }
+        }
+        return new Matrix(resultArray);
+    }
+
+    public int subtract(int n, int m){
+        return n - m;
+    }
+
+    public Matrix multiply(Matrix n, Matrix m) {
+        // If A is an n × m matrix and B is an m × p matrix
+        int r1 = n.getRows();
+        int r2 = m.getRows();
+        int c1 = n.getColumns();
+        int c2 = m.getColumns();
+        // exception when dimensions are wrong
+        if (c1 != r2) {
+            throw new RuntimeException("Illegal matrix dimensions.");
+        }
+
+        int[][] resultArray = new int[r1][c2];
+
+        // calculate
+        for (int i = 0; i < r1; i++) {
+            for (int j = 0; j < c2; j++) {
+                for (int k = 0; k < c1; k++) {
+                     resultArray[i][j] += n.getNumber(i, k) * m.getNumber(k, j);
+                }
+            }
+        }
+            return new Matrix(resultArray);
+    }
+
+    public int multiply(int n, int m){
+        return n * m;
+    }
+
+    public Matrix scalarMatrix(int c, Matrix m){
+        int[][] resultArray = new int[m.getRows()][m.getColumns()];
+        for(int row=0; row<m.getRows(); row++){
+            for(int col=0; col<m.getColumns(); col++){
+                resultArray[row][col] = c * m.getNumber(row, col);
+            }
+        }
+        return new Matrix(resultArray);
+    }
+
+    public Matrix transposeMatrix(Matrix m){
+        int newRows = m.getColumns();
+        int newCols = m.getRows();
+        int[][] resultArray = new int[newRows][newCols];
+
+        for(int row=0; row<m.getRows(); row++){
+            for(int col=0; col<m.getColumns(); col++){
+                resultArray[col][row] = m.getNumber(row, col);
+            }
+        }
+        return new Matrix(resultArray);
+    }
+
+    public Matrix power(Matrix m, int a){
+        if(a == 1){
+            return m;
+        }
+        else{
+            return (multiply(m, power(m, a-1)));
+        	
+        	//m = multiply(m, m);
+            //return power(m, a-1);
+        }
+    }
+
+    public int power(int a, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        else{
+            return (a * (power(a, n-1)));
+        }
+    }
+    
+    public int determinant(Matrix m) {
+    	return m.getDeterminant();
+    }
+    
+    public Matrix inverse(Matrix m) {
+    	
+    	int[][] x;
+    	
+    	if(determinant(m) == 0) {
+    		throw new RuntimeException("Non-Invertable Matrix");
+    	}
+    	if(m.getRows() == 2) {
+    		x = new int[2][2];
+    		x[0][0] = (m.getNumber(1, 1)/m.getDeterminant());
+    		x[0][1] = -(m.getNumber(0, 1)/m.getDeterminant());
+    		x[1][0] = -(m.getNumber(1, 0)/m.getDeterminant());
+    		x[1][1] = (m.getNumber(0, 0)/m.getDeterminant());
+    		return new Matrix(x);
+    	}
+    	return new Matrix(0);
+    }
+}
+
