2b1110ae01577cda9a325d15d749b641ce696ba6
FrankJonasmoelle <48800769+FrankJonasmoelle@users.noreply.github.com>
2019-04-30 10:09:07 -0400

    Add files via upload
    Including dot product and cross product

--- Calculate.java
+++ Calculate.java
@@ -1,9 +1,15 @@
 public class Calculate extends Matrix {
-	
-	public Calculate() {
-		super();
-	}
-	
+
+    public Calculate() {
+        super();
+    }
+
+    /**
+     *
+     * @param n
+     * @param m
+     * @return
+     */
     public Matrix add(Matrix n, Matrix m) {
         // Note: can only be added if same dimension!
         int[][] resultArray = new int[m.getRows()][m.getColumns()];
@@ -21,10 +27,22 @@ public class Calculate extends Matrix {
         return new Matrix(resultArray);
     }
 
+    /**
+     *
+     * @param n
+     * @param m
+     * @return
+     */
     public int add(int n, int m){
         return n + m;
     }
 
+    /**
+     *
+     * @param n
+     * @param m
+     * @return
+     */
     public Matrix subtract(Matrix n, Matrix m) {
         // Note: can only be added if same dimension!
         int[][] resultArray = new int[m.getRows()][m.getColumns()];
@@ -42,12 +60,24 @@ public class Calculate extends Matrix {
         return new Matrix(resultArray);
     }
 
+    /**
+     *
+     * @param n
+     * @param m
+     * @return
+     */
     public int subtract(int n, int m){
         return n - m;
     }
 
+    /**
+     *
+     * @param n
+     * @param m
+     * @return
+     */
     public Matrix multiply(Matrix n, Matrix m) {
-        // If A is an n × m matrix and B is an m × p matrix
+        // If A is an n Ã— m matrix and B is an m Ã— p matrix
         int r1 = n.getRows();
         int r2 = m.getRows();
         int c1 = n.getColumns();
@@ -63,17 +93,29 @@ public class Calculate extends Matrix {
         for (int i = 0; i < r1; i++) {
             for (int j = 0; j < c2; j++) {
                 for (int k = 0; k < c1; k++) {
-                     resultArray[i][j] += n.getNumber(i, k) * m.getNumber(k, j);
+                    resultArray[i][j] += n.getNumber(i, k) * m.getNumber(k, j);
                 }
             }
         }
-            return new Matrix(resultArray);
+        return new Matrix(resultArray);
     }
 
+    /**
+     *
+     * @param n
+     * @param m
+     * @return
+     */
     public int multiply(int n, int m){
         return n * m;
     }
 
+    /**
+     *
+     * @param c
+     * @param m
+     * @return
+     */
     public Matrix scalarMatrix(int c, Matrix m){
         int[][] resultArray = new int[m.getRows()][m.getColumns()];
         for(int row=0; row<m.getRows(); row++){
@@ -84,6 +126,11 @@ public class Calculate extends Matrix {
         return new Matrix(resultArray);
     }
 
+    /**
+     *
+     * @param m
+     * @return
+     */
     public Matrix transposeMatrix(Matrix m){
         int newRows = m.getColumns();
         int newCols = m.getRows();
@@ -97,18 +144,30 @@ public class Calculate extends Matrix {
         return new Matrix(resultArray);
     }
 
+    /**
+     *
+     * @param m
+     * @param a
+     * @return
+     */
     public Matrix power(Matrix m, int a){
         if(a == 1){
             return m;
         }
         else{
             return (multiply(m, power(m, a-1)));
-        	
-        	//m = multiply(m, m);
+
+            //m = multiply(m, m);
             //return power(m, a-1);
         }
     }
 
+    /**
+     *
+     * @param a
+     * @param n
+     * @return
+     */
     public int power(int a, int n) {
         if (n == 0) {
             return 1;
@@ -117,27 +176,74 @@ public class Calculate extends Matrix {
             return (a * (power(a, n-1)));
         }
     }
-    
+
+    /**
+     *
+     * @param m
+     * @return
+     */
     public int determinant(Matrix m) {
-    	return m.getDeterminant();
+        return m.getDeterminant();
     }
-    
-    public Matrix inverse(Matrix m) {
-    	
-    	int[][] x;
-    	
-    	if(determinant(m) == 0) {
-    		throw new RuntimeException("Non-Invertable Matrix");
-    	}
-    	if(m.getRows() == 2) {
-    		x = new int[2][2];
-    		x[0][0] = (m.getNumber(1, 1)/m.getDeterminant());
-    		x[0][1] = -(m.getNumber(0, 1)/m.getDeterminant());
-    		x[1][0] = -(m.getNumber(1, 0)/m.getDeterminant());
-    		x[1][1] = (m.getNumber(0, 0)/m.getDeterminant());
-    		return new Matrix(x);
-    	}
-    	return new Matrix(0);
+
+    /**
+     *
+     * @param n
+     * @param m
+     * @return
+     */
+    public int dotProduct(Matrix n, Matrix m){
+        int numColumnsN = n.getColumns();
+        int numColumnsM = m.getColumns();
+        int numRowsN = n.getRows();
+        int numRowsM = m.getRows();
+        int dotProduct = 0;
+
+        if((numColumnsN > 1)|| (numColumnsM > 1)){
+            throw new RuntimeException("Illegal matrix dimensions; columns need to be of dimension 1");
+        }
+        if(numRowsM != numRowsN){
+            throw new RuntimeException("Illegal matrix dimensions; vectors must have same amount of rows");
+        }
+
+        for(int i=0; i<m.getRows(); i++){
+            dotProduct += (n.getNumber(i, 0) * m.getNumber(i, 0));
+        }
+        return dotProduct;
+    }
+
+    /**
+     *
+     * @param m
+     * @param n
+     * @return
+     */
+    public Matrix crossProduct(Matrix m, Matrix n){
+        int numColumnsN = n.getColumns();
+        int numColumnsM = m.getColumns();
+        int numRowsN = n.getRows();
+        int numRowsM = m.getRows();
+
+        int[][] crossArr = new int[n.getRows()][n.getColumns()];
+
+        if((numColumnsN > 1)|| (numColumnsM > 1)){
+            throw new RuntimeException("Illegal matrix dimensions; columns need to be of dimension 1");
+        }
+        if(numRowsM != numRowsN){
+            throw new RuntimeException("Illegal matrix dimensions; vectors must have same amount of rows");
+        }
+        if(numRowsM != 3 || numRowsN != 3){
+            throw new RuntimeException("Illegal matrix dimensions; vector must be of dimension 3");
+        }
+
+        crossArr[0][0] = -(n.getNumber(1,0) * m.getNumber(2, 0)
+                - n.getNumber(2,0) * m.getNumber(1,0));
+        crossArr[1][0] = n.getNumber(0,0) * m.getNumber(2,0)
+                - n.getNumber(2,0) * m.getNumber(0, 0) ;
+        crossArr[2][0] = -(n.getNumber(0, 0) * m.getNumber(1,0)
+                - n.getNumber(1,0) * m.getNumber(0,0));
+
+        return new Matrix(crossArr);
     }
 }
 
